0d25cc5709d84c54095c7bd74709a5ce11c9cc44
yamkarki99 <99rajeevkarki@gmail.com>
2023-05-21 13:40:55 +1000

    contact api

---
+++ script.js
@@ -0,0 +1,91 @@
+// work flow
+
+//1. fetch user from api
+//2. store those user in global array
+//3. disply user in the UI
+
+let userList = [];
+const apiEp = "https://randomuser.me/api?";
+
+const displayElm = document.getElementById("list");
+const fetchUsers = async (path = "results=20") => {
+  // Promise
+  //   fetch(apiEp)
+  //     .then((response) => {
+  //       return response.json();
+  //     })
+  //     .then((data) => {
+  //       userList = data.results;
+  //       console.log(data);
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+
+  // async/await
+  try {
+    const response = await fetch(apiEp + path);
+    const data = await response.json();
+
+    userList = data.results;
+    console.log(data.results);
+    displyUser();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+fetchUsers();
+
+const displyUser = () => {
+  let str = "";
+  userList.forEach((usr) => {
+    str += `
+    <div class="card" style="width: 18rem;">
+  <img src="${usr?.picture?.large}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${usr?.name?.title} ${usr?.name?.first} ${usr?.name?.last}</h5>
+    <div class="card-text">
+    
+    <div>
+    <i class="fa-solid fa-envelope"></i>
+    ${usr?.email}
+    </div>
+    <div>
+    <i class="fa-solid fa-map"></i>
+    ${usr?.location?.street?.number}  ${usr?.location?.street?.name} ${usr?.location?.city} ${usr?.location?.country}
+    </div>
+    </div>
+    <a href="tel:${usr?.phone}"> 
+    <div class="d-grid">    
+    <button class="btn btn-primary">
+    <i class="fa-solid fa-phone"></i>
+    ${usr?.phone} </button>
+  
+    </div>
+    </a>
+  </div>
+</div>`;
+  });
+
+  displayElm.innerHTML = str;
+  countElm.innerText = displayArg.length;
+};
+
+document.getElementById("select").addEventListener("change", (e) => {
+  const { value } = e.target;
+  const path = `results=20&gender=` + value;
+  fetchUsers(path);
+});
+
+document.getElementById("search-input").addEventListener("keyup", (e) => {
+  const { value } = e.target;
+  // run filter method
+  const filteredUser = userList.filter((item) => {
+    console.log(item);
+    const fullName = item.name.first + "" + item.name.last;
+    return fullName.includes(value.toLowercase());
+  });
+  displayUser(filteredUser);
+  // display function
+});
